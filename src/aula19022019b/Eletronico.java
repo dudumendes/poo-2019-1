11a77306433bb8059042b86b97af77a44f53a010
Eduardo Mendes <eduardomendes@Eduardos-MacBook-Pro.local>
2019-02-21 21:53:29 -0300

    exemplificacao de lacos for com ArrayList

--- src/aula19022019b/Eletronico.java
+++ src/aula19022019b/Eletronico.java
@@ -3,4 +3,8 @@ package aula19022019b;
 public class Eletronico {
 	String marca;
 	String modelo;
+	
+	public String toString() {
+		return marca + ": " + modelo;
+	}
 }
--- src/aula19022019b/Main.java
+++ src/aula19022019b/Main.java
@@ -8,5 +8,17 @@ public class Main {
 		c1.perifericos.add(new Teclado("Apple", "AppleKey"));
 		c1.perifericos.add(new Mouse("Apple", "Magic Mouse"));
 		c1.perifericos.add(new Monitor("AOC", "Curve"));
+		
+		for (int i = 0; i < c1.perifericos.size(); i++) {
+			System.out.println(c1.perifericos.get(i));
+		}
+		
+		//Declaro uma variavel do tipo dos elementos
+		//da minha lista e informo qual a lista
+		for (Periferico pf : c1.perifericos) {
+			System.out.println(pf);
+		}
+		
+		c1.perifericos.forEach(p1 -> System.out.println(p1));
 	}
 }
